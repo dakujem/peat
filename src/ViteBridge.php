e2efda56332196b004ac622038130c0f5dd2c0bb
Andrej Rypo <xrypak@gmail.com>
2025-06-25 18:32:37 +0200

    Splits ViteBridge::makePassiveEntryLocator into two separate methods
    - adds ViteBridge::makeDevServerEntryLocator method
    - adds ViteBridge::makeBundleEntryLocator method

--- src/ViteBridge.php
+++ src/ViteBridge.php
@@ -9,7 +9,7 @@ use RuntimeException;
 
 /**
  * A factory service providing Vite entry locators.
- * Also provides capability to populate a PHP cache file.
+ * Also provides an ability to populate a PHP cache file for performance improvement.
  *
  * @author Andrej Rypak <xrypak@gmail.com>
  */
@@ -49,18 +49,39 @@ final class ViteBridge
      * The locator reads the manifest file (or cache file) and serves asset objects.
      *
      * If the $useDevServer is `true`, links to Vite dev server are returned by the locator.
+     *
+     * This call does not detect whether the dev server is actually running or not (hence "passive").
      */
     public function makePassiveEntryLocator(
         bool $useDevServer = false
     ): ViteLocatorContract {
-        // If the dev server is used, all assets are served by the server.
+        // If the dev server is used, it serves all the assets.
         if ($useDevServer) {
-            if ($this->devServerUrl === null) {
-                throw new LogicException('The development server URL has not been provided.');
-            }
-            return new ViteServerLocator($this->devServerUrl);
+            return $this->makeDevServerEntryLocator();
         }
         // Otherwise, the assets are served from a bundle (build).
+        return $this->makeBundleEntryLocator();
+    }
+
+    /**
+     * Returns a preconfigured asset entry locator for development.
+     * The locator returns entries pointing to the Vite development server for any asset being resolved.
+     */
+    public function makeDevServerEntryLocator(): ViteLocatorContract
+    {
+        // If the dev server is used, all assets are served by the server.
+        if ($this->devServerUrl === null) {
+            throw new LogicException('The development server URL has not been provided.');
+        }
+        return new ViteServerLocator($this->devServerUrl);
+    }
+
+    /**
+     * Returns a preconfigured asset entry locator for production builds.
+     * The locator reads the manifest file (or cache file) and serves asset objects.
+     */
+    public function makeBundleEntryLocator(): ViteLocatorContract
+    {
         $bundleLocator = new ViteBuildLocator(
             $this->manifestFile,
             $this->cacheFile,
@@ -80,7 +101,7 @@ final class ViteBridge
     }
 
     /**
-     * Populates a cache file that is included instead of parsing a JSON manifest.
+     * Populates a cache file to be included instead of parsing the Vite bundle's JSON manifest file.
      * Should be called during the deployment/CI process as one of the build steps.
      */
     public function populateCache(): void
